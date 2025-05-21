174e6aff1f21781d56ce706fa6c656010b7f5f59
Bordovichek <slava.khomiakov2019@gmail.com>
2025-05-21 14:36:56 +0300

    1) Добавлены сотни аэропортов 2) Модифицирован сбор данных об аэропортах 3) Добавлена карта с выбором аэропортов

--- MPS_Project/web/urls.py
+++ MPS_Project/web/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 from core.views import home
+from web.views import flight_view
 
 urlpatterns = [
     path('', home, name='home'),
     path('', include('core.urls')),
+    path("map/", flight_view, name="map"),
 ]
