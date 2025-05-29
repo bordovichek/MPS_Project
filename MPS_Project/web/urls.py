cc07a28cc7a83bfc0f4871edddd842daa83dadcc
Bordovichek <slava.khomiakov2019@gmail.com>
2025-05-29 23:37:04 +0300

    1) Сделаны вьюхи для отдельно самолетов и аэропортов 2) Сделаны фронтовские заглушки, надо допилить до нормального вида 3) Переход по координатам аэропорта на гугл карты

--- MPS_Project/web/urls.py
+++ MPS_Project/web/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from core.views import home
+from web import views
 from web.views import flight_view, airport_list_view, airplane_list_view
 
 urlpatterns = [
     path('core/', include('core.urls')),
+    path('airports/<int:pk>/', views.AirportDetailView.as_view(), name='airport_detail'),
+    path('airplanes/<int:pk>/', views.AirplaneDetailView.as_view(), name='airplane_detail'),
     path('airplanes/', airplane_list_view, name='airplane_list'),
     path('airports/', airport_list_view, name='airport_list'),
     path('map/', flight_view, name="map"),
--- MPS_Project/web/views.py
+++ MPS_Project/web/views.py
@@ -1,4 +1,7 @@
+from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 from django.shortcuts import render
+from django.db import models
+from django.views.generic import DetailView
 
 from core.models_dir import Airport, Airplane
 
@@ -22,4 +25,45 @@ def airplane_list_view(request):
 
 def airport_list_view(request):
     airports = Airport.objects.all()
-    return render(request, 'web/list_airports.html', {'airports': airports})
+
+    search_query = request.GET.get('search')
+    if search_query:
+        airports = airports.filter(
+            models.Q(name__icontains=search_query) | models.Q(iata_code__icontains=search_query)
+        ).distinct()
+    sort_by = request.GET.get('sort_by', 'name')
+    order_by_fields = []
+
+    if sort_by == 'name':
+        order_by_fields.append('name')
+    elif sort_by == 'iata_code':
+        order_by_fields.append('iata_code')
+    elif sort_by == 'country':
+        order_by_fields.append('country')
+        order_by_fields.append('name')
+    airports = airports.order_by(*order_by_fields)
+
+    paginator = Paginator(airports, 100)
+    page_number = request.GET.get('page')
+    try:
+        page_obj = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        page_obj = paginator.get_page(1)
+    except EmptyPage:
+        page_obj = paginator.get_page(paginator.num_pages)
+
+    return render(request, 'web/list_airports.html', {
+        'airports': page_obj,
+        'search_query': search_query,
+        'sort_by': sort_by,
+    })
+
+class AirportDetailView(DetailView):
+    model = Airport
+    template_name = 'web/airport_detail.html'
+    context_object_name = 'airport'
+
+class AirplaneDetailView(DetailView):
+    model = Airplane
+    template_name = 'web/airplane_detail.html'
+    context_object_name = 'airplane'
